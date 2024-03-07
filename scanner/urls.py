301bd0c5ae9488be056a64ccbdfda47e88a17158
MohitSingh65 <mohitsinghpanwar65@gmail.com>
2024-03-07 18:45:39 +0530

    Updation

--- scanner/urls.py
+++ scanner/urls.py
@@ -1,10 +1,12 @@
 # scanner/urls.py
 
 from django.urls import path
+from authentication import views
 from .views import index, scan, download
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('scan/', scan, name='scan'),
     path('download/', download, name='download'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
